2d20a6e220686fd74c25dc0e874634f75139fd12
Felicia Thorne <feliciathorne@Felicias-MacBook-Pro.local>
2018-01-11 09:56:31 -0800

    updated logic to loop through array, more effective, removed repetitive code

--- controllers/coin-controller.js
+++ controllers/coin-controller.js
@@ -13,39 +13,42 @@ function Coin(id, name, imgUrl, price, marketCap, change24, trend) {
     this.trend = trend;
 }
 
-function getPage1() {
-    return axios.get('https://coinranking.com/?page=1')
-}
-
-function getPage2() {
-    return axios.get('https://coinranking.com/?page=2')
-}
-
-function getPage3() {
-    return axios.get('https://coinranking.com/?page=3')
-}
 
 var coinArr = [];
+var id = 0;
+
+var pages = [
+    'https://coinranking.com/?page=1', 
+    'https://coinranking.com/?page=2',
+    'https://coinranking.com/?page=3'
+]
 
 function scrape (req, res) {
 
-    axios.all([getPage1(), getPage2(), getPage3()])
-        .then(axios.spread(function (page1, page2, page3) {
+    axios.all(pages.map(p => axios.get(p)))
+        .then(axios.spread(function(...response) {
 
-                var $ = cheerio.load(page1.data)
+            var pageArr = [];
+
+            for (var i = 0; i < response.length; i++) {
+                pageArr.push(response[i].data)
+            }
+
+            for (var i = 0; i < pageArr.length; i++) {
+
+                var $ = cheerio.load(pageArr[i])
                 console.log("logging $: ", $)
 
-                var id = 0;
                 var name$ = $('.coin-name')
                 var imgUrl$ = $('.coin-list__body__row__cryptocurrency__prepend__icon__img')
                 var price$ = $('.coin-list__body__row__price__value')
                 var marketCap$ = $('.coin-list__body__row__market-cap__value')
                 var change24$ = $('.coin-list__body__row__change')
                 var trend = null;
-                
-                for (var i = 0; i < name$.length; i++) {
+
+                for (var j = 0; j < name$.length; j++) {
                     console.log('inside for loop')
-                    if ($(change24$[i]).attr('class').includes("negative")) {
+                    if ($(change24$[j]).attr('class').includes("negative")) {
                         console.log('negative')
                         trend = "negative"
                     } else {
@@ -54,69 +57,16 @@ function scrape (req, res) {
                     }
                     var newCoin = new Coin(
                         id++,
-                        $(name$[i]).text(),
-                        $(imgUrl$[i]).attr('src'),
-                        parseFloat($(price$[i]).text().replace(/,/g, "")),
-                        parseFloat($(marketCap$[i]).text().replace(/,/g, "")),
-                        $(change24$[i]).text().trim(),
+                        $(name$[j]).text(),
+                        $(imgUrl$[j]).attr('src'),
+                        parseFloat($(price$[j]).text().replace(/,/g, "")),
+                        parseFloat($(marketCap$[j]).text().replace(/,/g, "")),
+                        $(change24$[j]).text().trim(),
                         trend
                     )
                     coinArr.push(newCoin)
                 }
-
-                var $ = cheerio.load(page2.data)
-
-                var name$ = $('.coin-name')
-                var imgUrl$ = $('.coin-list__body__row__cryptocurrency__prepend__icon__img')
-                var price$ = $('.coin-list__body__row__price__value')
-                var marketCap$ = $('.coin-list__body__row__market-cap__value')
-                var change24$ = $('.coin-list__body__row__change')
-                var trend = null;
-
-                for (var i = 0; i < name$.length; i++) {
-                    if ($(change24$[i]).attr('class').includes("negative")) {
-                        trend = "negative"
-                    } else {
-                        trend = "up"
-                    }
-                    var newCoin = new Coin(
-                        id++,
-                        $(name$[i]).text(),
-                        $(imgUrl$[i]).attr('src'),
-                        parseFloat($(price$[i]).text().replace(/,/g, "")),
-                        parseFloat($(marketCap$[i]).text().replace(/,/g, "")),
-                        $(change24$[i]).text().trim(),
-                        trend
-                    )
-                    coinArr.push(newCoin)
-                }
-
-                var $ = cheerio.load(page3.data)
-
-                var name$ = $('.coin-name')
-                var imgUrl$ = $('.coin-list__body__row__cryptocurrency__prepend__icon__img')
-                var price$ = $('.coin-list__body__row__price__value')
-                var marketCap$ = $('.coin-list__body__row__market-cap__value')
-                var change24$ = $('.coin-list__body__row__change')
-                var trend = null;
-
-                for (var i = 0; i < name$.length; i++) {
-                    if ($(change24$[i]).attr('class').includes("negative")) {
-                        trend = "negative"
-                    } else {
-                        trend = "up"
-                    }
-                    var newCoin = new Coin(
-                        id++,
-                        $(name$[i]).text(),
-                        $(imgUrl$[i]).attr('src'),
-                        parseFloat($(price$[i]).text().replace(/,/g, "")),
-                        parseFloat($(marketCap$[i]).text().replace(/,/g, "")),
-                        $(change24$[i]).text().trim(),
-                        trend
-                    )
-                    coinArr.push(newCoin)
-                }
+            }
                 res.json(coinArr)
             }))
             .catch(function (error) {
